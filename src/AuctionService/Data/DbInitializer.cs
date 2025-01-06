c1db2f227b764bfd0a90ce3b9106692dfac0a024
krutiksheth <sheth.krutik@gmail.com>
2025-01-06 16:02:52 +0530

    Configured entity framework

---
+++ src/AuctionService/Data/DbInitializer.cs
@@ -0,0 +1,9 @@
+﻿namespace AuctionService.Data;
+
+public class DbInitializer
+{
+    public static void InitDb(WebApplication app)
+    {
+        
+    }
+}
--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,4 +1,7 @@
+using AuctionService.Data;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
+
+//Configure Entity Framework
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
